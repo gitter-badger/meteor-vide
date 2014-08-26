49e4a19564e00076de300e88c6b429e9e69a0432
zimme <zimme@users.noreply.github.com>
2014-08-26 22:35:56 +0200

    Migrate to new package system

--- package.js
+++ package.js
@@ -1,9 +1,13 @@
 Package.describe({
-  summary: "Easy as hell jQuery plugin for video backgrounds",
-  version: "0.0.1"
+  summary: 'Easy as hell jQuery plugin for video backgrounds',
+  name: 'zimme'
+  git: 'https://github.com/zimme/meteor-vide.git',
+  version: '0.0.1'
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom('METEOR@0.9.0');
+
   api.use('jquery', 'client');
 
   api.add_files('Vide/dist/jquery.vide.js', 'client');
