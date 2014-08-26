76288991a5a6c70ddd34940003795442832d457b
zimme <zimme@users.noreply.github.com>
2014-08-26 22:42:27 +0200

    Version bump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: 'Easy as hell jQuery plugin for video backgrounds',
   name: 'zimme:vide',
   git: 'https://github.com/zimme/meteor-vide.git',
-  version: '0.0.1'
+  version: '0.0.2'
 });
 
 Package.on_use(function (api, where) {
